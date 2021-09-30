e1ab0f08a90b199efae665bc6c9fdf2c25e897f9
juand <domingoortin.j@gmail.com>
2021-09-30 18:19:55 +0200

    Download step 4 of dice roll project

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        // TODO (04) Choose the right drawable resource based on the value of randomInt
+        // Tip: Use a when expression
+
+        // TODO (03) Get a reference to the imageView instead
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        // TODO (05) Make the ImageView show the chosen drawable resource
+        resultText.text = randomInt.toString()
+    }
+}
