df99ba98fe0607d9c486cd7669186213b50aca68
juand <domingoortin.j@gmail.com>
2021-10-01 20:58:12 +0200

    My tests

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -19,6 +19,7 @@ package com.example.android.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.diceroller.databinding.ActivityMainBinding
